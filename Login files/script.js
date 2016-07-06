0ec30a2ac6bae9986051b8ae6337a341305fe913
ankit0905 <ankit0905anand@gmail.com>
2016-07-05 20:03:34 -0700

    Basic Layout for login - will change accordingly

---
+++ Login files/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.tab a').click(function(){
+
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+        
+        var target = $(this).attr('href');
+        $('.content > div').not(target).hide();
+        $(target).fadeIn(600);
+    })
+})
