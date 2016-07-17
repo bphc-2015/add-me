953c61f47bc08b141cb97da1beedc7b232e5106b
ankit0905 <ankit0905anand@gmail.com>
2016-07-17 22:33:16 +0530

    added database model file

---
+++ qr/public/merged-login&home/database-model.js
@@ -0,0 +1,31 @@
+/*MongoDB model for Storing group and Contacts Data*/
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');   //test will be replaced by the database location
+
+
+var contactSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    phone: Number,
+    updated_at:{type: Date, default: Date.now},
+});
+
+var contacts = mongoose.model('contacts', contactSchema);
+
+var groupSchema = new mongoose.Schema({
+    name: String,
+    members: Array  //members field is actually array of contacts
+});
+
+var groups = mongoose.model('groups', groupSchema);
+
+var callback = function(err, data){
+    if(err) return console.error(err);
+    console.log(data);
+}
+
+/*
+example: groups.find({name:"someName"}, callback);
+         contacts.find({phone: Number}, callback);
+*/
